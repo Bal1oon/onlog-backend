d51817e127e84bb9d2bd6c3ddb3f8b92701169e5
Bal1oon <ws32159@naver.com>
2024-09-07 20:52:16 +0900

    fix: Update UserModule to import and provide Repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UserRepository]
 })
 export class UsersModule {}
