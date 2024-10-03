ed81df1cda9ac0fe58166cc4479c1f304d91d65f
Bal1oon <ws32159@naver.com>
2024-10-03 23:43:57 +0900

    feat: Add logging on CategoryService

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryRepository } from './category.repository';
 import { CategoryRequestDto } from './dto/category-request.dto';
@@ -8,6 +8,7 @@ import { UserRepository } from 'src/users/user.repository';
 
 @Injectable()
 export class CategoryService {
+    private logger = new Logger('CategoryService');
     constructor(
         @InjectRepository(CategoryRepository)
         private readonly categoryRepository: CategoryRepository,
@@ -30,6 +31,8 @@ export class CategoryService {
         const category = this.categoryRepository.create({ name: name.trim(), user });
         await this.categoryRepository.save(category);
 
+        this.logger.log(`User '${user.id}' created category - id: ${category.id}, name: ${name}`);
+
         return category;
     }
 
@@ -40,15 +43,18 @@ export class CategoryService {
     async deleteCategory(id: number, user: User): Promise<void> {
         const category = await this.categoryRepository.getCategoryWithPost(id);
         if (!category) {
+            this.logger.warn(`User '${user.id}' attempted to delete a non-existing category - id: ${id}`);
             throw new NotFoundException('Category not found');
         }
 
         if (!this.isOwn(category.user.id, user.id)) {
+            this.logger.warn(`User '${user.id}' attempted to delete category - id: ${id}, but does not own it`);
             throw new BadRequestException('You can only manage your own');
         }
 
         this.hasPosts(category);
         await this.categoryRepository.delete(id);
+        this.logger.log(`User '${user.id}' deleted category - id: ${category.id}, name: '${category.name}'`);
     }
 
     private hasPosts(category: Category): void {
@@ -61,16 +67,21 @@ export class CategoryService {
     async updateCategory(id: number, categoryRequestDto: CategoryRequestDto, user: User): Promise<Category> {
         const category = await this.categoryRepository.getCategoryWithPost(id);
         if (!category) {
+            this.logger.warn(`User '${user.id}' attempted to update a non-existing category - id: ${id}`);
             throw new NotFoundException('Category not found');
         }
 
         if (!this.isOwn(category.user.id, user.id)) {
+            this.logger.warn(`User '${user.id}' attempted to update category - id: ${id}, but does not own it`);
             throw new BadRequestException('You can only manage your own');
         }
 
         const { name } = categoryRequestDto;
+        const beforeName = category.name;
         category.name = name;
         await this.categoryRepository.save(category);
+
+        this.logger.log(`User '${user.id}' updated category to ${name} from ${beforeName}`);
 
         return category;
     }
